ed1bd7eacfcd71278db9f89baf77d87d55feba26
Adir7SK <31063975+Adir7SK@users.noreply.github.com>
2018-06-07 10:36:25 +0200

    Update benchmark_el.py

--- asv_benchmark/benchmarks/benchmark_el.py
+++ asv_benchmark/benchmarks/benchmark_el.py
@@ -1,12 +1,15 @@
-import timeit
 import qcodes
 from qcodes.instrument_drivers.Keysight.Keysight_34465A import Keysight_34465A
+import timeit
 
 class TimeSuite:
     """
     An example benchmark that times the performance of various kinds
     of iterating over dictionaries in Python.
     """
+    timeout=120
+    timer = timeit.default_timer
+    
     def setup(self):
         self.key = Keysight_34465A('name', 'USB0::0x2A8D::0x0101::MY54505388::INSTR')
 
@@ -15,3 +18,11 @@ class TimeSuite:
 
     def time_keyEl(self):
         self.key.NPLC.get()
+
+    def time_keyEl100(self):
+        self.key.NPLC(100)
+        self.key.volt()
+
+    def time_keyEl10(self):
+        self.key.NPLC(10)
+        self.key.volt()
